e1ed36b7c740b907c594101d9a2c710143a38855
Deepanxhu <deepanshu1.yadav@gmail.com>
2024-10-01 15:13:44 +0530

    Minor Visual Modifications

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -1,32 +1,36 @@
-import React from 'react'
+import React from "react";
 import amazon from "../assets/amazon.png";
 import flipkart from "../assets/flipkart.png";
 import shoe_img from "../assets/shoe_image.png";
 
 function Hero() {
   return (
-  <main className='hero'>
-    <div className='hero-content'>
+    <main className="hero">
+      <div className="hero-content">
         <h1>YOUR FEET DESERVE THE BEST</h1>
-        <p>YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
-    
-    <div className='hero-btn'>
-        <button>Shop Now</button>
-        <button className='s-btn'>Category</button>
-    </div>
-    <div className='shopping'>
-        <p>Also Available on</p>
-        <div className='brand-icons'>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.
+        </p>
+
+        <div className="hero-btn">
+          <button>Shop Now</button>
+          <button className="s-btn">Category</button>
+        </div>
+        <div className="shopping">
+          <p>Also Available on</p>
+          <div className="brand-icons">
             <img src={amazon} alt="amazon-logo" />
             <img src={flipkart} alt="flikpart-logo" />
+          </div>
         </div>
-    </div>
-    </div>
-    <div className='hero-image'>
+      </div>
+      <div className="hero-image">
         <img src={shoe_img} alt="hero-img" />
-    </div>
+      </div>
     </main>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import brand_logo from "../assets/brand_logo.png"
+import React from "react";
+import brand_logo from "../assets/brand_logo.png";
 
 function Navbar() {
   return (
-      <nav>
-        <div className='logo' >
-            <img src={brand_logo} alt="logo" />
-        </div>
+    <nav>
+      <div className="logo">
+        <img src={brand_logo} alt="logo" />
+      </div>
 
-        <ul>
-            <li href='#'>MENU</li>
-            <li href='#'>LOCATION</li>
-            <li href='#'>ABOUT</li>
-            <li href='#'>CONTACT</li>
-        </ul>
-        <button>LOGIN</button>
-      </nav>
-
-  )
+      <ul>
+        <li href="#">MENU</li>
+        <li href="#">LOCATION</li>
+        <li href="#">ABOUT</li>
+        <li href="#">CONTACT</li>
+      </ul>
+      <button>LOGIN</button>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
